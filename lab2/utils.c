05d848d52dc02ce0a385296597181c76fd33e325
Maria Vieira <up202204802@up.pt>
2025-02-24 15:33:21 +0000

    util_sys_inb explained

--- lab2/utils.c
+++ lab2/utils.c
@@ -16,8 +16,14 @@ int(util_get_MSB)(uint16_t val, uint8_t *msb) {
   return 1;
 }
 
+
+// if we use the address of a uint8_t variable, 
+// the program may terminate abnormally, because it 
+// accesses a memory position at an invalid address 
+// (thus triggering a segmentation violation signal)
+// for that reason, we use util_sys_inb
 int (util_sys_inb)(int port, uint8_t *value) {
-  if (value==NULL) return 1;
+  if (value==NULL) return 1;   // 1 = erro
   uint32_t val;
   int ret = sys_inb(port, &val);
 
