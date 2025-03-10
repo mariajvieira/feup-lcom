1a8dfb30355425c104a567a2031963a9f60c0935
Duarte Marques <up202204973@up.pt>
2025-03-10 15:40:47 +0000

    timer_get_conf - timer.c

--- lab2/timer.c
+++ lab2/timer.c
@@ -6,10 +6,8 @@
 #include "i8254.h"
 
 int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  if(timer < 0 || timer > 2) return 1;
+  if(freq < 19 || freq > TIMER_FREQ) return 1;
 }
 
 int (timer_subscribe_int)(uint8_t *bit_no) {
@@ -32,10 +30,12 @@ void (timer_int_handler)() {
 }
 
 int (timer_get_conf)(uint8_t timer, uint8_t *st) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  if(st==NULL) return 1;
+  if(timer < 0 || timer > 2) return 1;
+  uint8_t readback = TIMER_RB_CMD | TIMER_RB_COUNT_ | TIMER_RB_SEL(timer);
+  if(sys_outb(TIMER_CTRL, readback) != 0) return 1;
+  if(util_sys_inb(TIMER_0 + timer, st) != 0) return 1;
+  return 0;
 }
 
 int (timer_display_conf)(uint8_t timer, uint8_t st,
